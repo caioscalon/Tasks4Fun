6e558d52ddfab4f6ec58c4ff17a78f6405849359
Fabricio Pirini <fabricio.pirini@gmail.com>
2018-05-21 11:56:32 -0300

    Login implementado (incompleto) + minSDKversion 25

---
+++ app/src/main/java/com/exampledemo/parsaniahardik/myapplication/Login.java
@@ -0,0 +1,85 @@
+package com.exampledemo.parsaniahardik.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
+
+public class Login extends Activity implements View.OnClickListener {
+
+    private static final int RC_SIGN_IN = 0;
+    private FirebaseAuth auth;
+    private RecyclerView mMainList;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mMainList = (RecyclerView) findViewById(R.id.recycler);
+
+        auth = FirebaseAuth.getInstance();
+
+        if(auth.getCurrentUser() != null){
+            //user already signed in
+            Log.d("AUTH", auth.getCurrentUser().getEmail());
+            TextView textoUser = (TextView) findViewById(R.id.user);
+            textoUser.setText(auth.getCurrentUser().getEmail());
+        }
+        else {
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(Arrays.asList(
+                                    new AuthUI.IdpConfig.EmailBuilder().build(),
+//                                    new AuthUI.IdpConfig.FacebookBuilder().build(),
+                                    new AuthUI.IdpConfig.GoogleBuilder().build()))
+                            .build(),
+                    RC_SIGN_IN);
+        }
+        findViewById(R.id.log_out_button).setOnClickListener(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RC_SIGN_IN){
+            if(resultCode == RESULT_OK){
+                //user logged in
+                Log.d("AUTH", auth.getCurrentUser().getEmail());
+            }
+            else{
+                //User not authenticated
+                Log.d("AUTH","Autenticação falhou");
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.log_out_button){
+            AuthUI.getInstance()
+                    .signOut(this)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            Log.d("AUTH", "USER DESCONECTADO");
+                            finish();
+                        }
+                    });
+        }
+    }
+
+}
