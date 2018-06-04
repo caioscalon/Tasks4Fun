91f2515636c5d8c0c5c0db6941e4d915bace01f3
Fabricio Pirini <fabricio.pirini@gmail.com>
2018-06-04 09:05:49 -0300

    Login implementado + Info user

--- app/src/main/java/com/exampledemo/parsaniahardik/myapplication/MainActivity.java
+++ app/src/main/java/com/exampledemo/parsaniahardik/myapplication/MainActivity.java
@@ -1,9 +1,12 @@
 package com.exampledemo.parsaniahardik.myapplication;
 
 import android.content.Context;
+import android.content.Intent;
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,9 +15,17 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.util.Log;
 
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 class Model {
 
@@ -43,6 +54,7 @@ class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHolder> {
     private LayoutInflater inflater;
     public static ArrayList<Model> imageModelArrayList;
     private Context ctx;
+
 
     public CustomAdapter(Context ctx, ArrayList<Model> imageModelArrayList) {
 
@@ -104,7 +116,7 @@ class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHolder> {
     }
 }
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
     private RecyclerView recyclerView;                          // Layout (Recycler View)
     private ArrayList<Model> modelArrayList;
@@ -113,6 +125,9 @@ public class MainActivity extends AppCompatActivity {
     private List<String> lista_ativ = new ArrayList<String>();  // Lista de atividades
     private int i = 0;
     private ArrayList<Model> list;
+    private static final int RC_SIGN_IN = 0;
+    private FirebaseAuth auth;
+    private RecyclerView mMainList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -130,6 +145,29 @@ public class MainActivity extends AppCompatActivity {
         customAdapter = new CustomAdapter(this,modelArrayList);
         recyclerView.setAdapter(customAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false));
+
+        mMainList = (RecyclerView) findViewById(R.id.recycler);
+
+        auth = FirebaseAuth.getInstance();
+
+        if(auth.getCurrentUser() != null){
+            //user already signed in
+            Log.d("AUTH", auth.getCurrentUser().getEmail());
+            TextView textoUser = (TextView) findViewById(R.id.user);
+            textoUser.setText(auth.getCurrentUser().getDisplayName());
+        }
+        else {
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(Arrays.asList(
+                                    new AuthUI.IdpConfig.EmailBuilder().build(),
+//                                    new AuthUI.IdpConfig.FacebookBuilder().build(),
+                                    new AuthUI.IdpConfig.GoogleBuilder().build()))
+                            .build(),
+                    RC_SIGN_IN);
+        }
+        findViewById(R.id.log_out_button).setOnClickListener(this);
 
         btnselect.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -186,6 +224,38 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RC_SIGN_IN){
+            if(resultCode == RESULT_OK){
+                //user logged in
+                Log.d("AUTH", auth.getCurrentUser().getEmail());
+                TextView textoUser = (TextView) findViewById(R.id.user);
+                textoUser.setText(auth.getCurrentUser().getDisplayName());
+            }
+            else{
+                //User not authenticated
+                Log.d("AUTH","Autenticação falhou");
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.log_out_button){
+            com.firebase.ui.auth.AuthUI.getInstance()
+                    .signOut(this)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            Log.d("AUTH", "USER DESCONECTADO");
+                            finish();
+                        }
+                    });
+        }
     }
 
     private ArrayList<Model> getModel(boolean isSelect){
